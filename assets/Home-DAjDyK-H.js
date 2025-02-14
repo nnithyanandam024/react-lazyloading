85a1066190fefff3fd39824c45b6cc6962e46790
nnithyanandam24 <nnithyanandam24@gmail.com>
2025-02-14 22:09:28 +0530

    Updates

---
+++ assets/Home-DAjDyK-H.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-nPJLGxE8.js";import"./vendor-DsOszMbr.js";function i(){return e.jsx("div",{className:"text",children:"This is Home page"})}export{i as default};
---
+++ assets/Page3-CdVhYlOH.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-nPJLGxE8.js";import"./vendor-DsOszMbr.js";function i(){return t.jsx("div",{className:"text",children:"This is Page 3"})}export{i as default};
