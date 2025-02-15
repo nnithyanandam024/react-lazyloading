b60cb37729682325f761020163b28595aaeb6d74
nnithyanandam24 <nnithyanandam24@gmail.com>
2025-02-15 10:58:19 +0530

    Updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReloadButton from '../pages/Reloadbutton'
 
 export default function Home() {
@@ -6,6 +6,9 @@ export default function Home() {
     <div className='text'>
       This is Home page
       <br />
+      <Suspense fallback={<div>Loading...</div>}>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis voluptates culpa sequi voluptas enim quod illum eveniet. Odit, blanditiis. Voluptatem illum quae, sit odio corporis provident necessitatibus cum asperiores voluptate sapiente dignissimos nam suscipit error. Rem dicta, quod aut labore mollitia consequatur sequi doloribus at nam ad, commodi doloremque ducimus?
+      </Suspense>
       <ReloadButton />
     </div>
   )
--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 
 export default function Page1() {
   return (
     <div className='text'>
-      This is Page 1
+      This is Page 1 <br />
+      <Suspense fallback={<div>Loading...</div>}>
+       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores et, iure, accusamus optio minus velit, sapiente quasi similique vitae mollitia esse? Unde perspiciatis corrupti incidunt! Molestiae culpa, iure dignissimos quod soluta qui dolorum non alias accusamus dolorem voluptatum. Magnam esse in eveniet debitis deleniti assumenda exercitationem illum non explicabo facilis.
+       </Suspense>
     </div>
   )
 }
