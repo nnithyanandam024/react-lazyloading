3638cc78b8a92f2b083b4ee25af51022e62ef3c3
nnithyanandam24 <nnithyanandam24@gmail.com>
2025-02-15 08:56:08 +0530

    Updates

--- assets/Page1-DMcwm1g4.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-CBhdjyMp.js";import"./vendor-DsOszMbr.js";function i(){return e.jsx(Suspense,{fallback:e.jsx("div",{className:"text",children:"Loading..."}),children:e.jsx("div",{className:"text",children:"This is Page 1"})})}export{i as default};
